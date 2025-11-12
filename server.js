431e4feae708cf5254280699d9480bedb108ed4b
mx2240 <lovemicky10@gmail.com>
2025-11-12 14:23:40 +0000

    up

--- server.js
+++ server.js
@@ -15,5 +15,10 @@ app.use(express.json());
 // Routes
 app.use("/api/auth", authRoutes);
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`✅ Server running on http://localhost:${PORT}`));
+
+app.get("/", (req, res) => {
+    res.send("✅ School API is running...");
+});
+
